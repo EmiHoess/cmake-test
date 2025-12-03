14c070f7895bc4eddb74f2866b3831c293d08aa7
Emi Hoess <emihoess@gmail.com>
2025-12-03 13:02:59 -0300

    dev: utest example

--- utest_minimal.cpp
+++ utest_minimal.cpp
@@ -5,7 +5,11 @@ int add(int, int);
 int main() {
     using namespace boost::ut;
 
-    "basic addition"_test = [] {
+    "basicc addition"_test = [] {
         expect(add(2,3) == 5_i);
+    };
+
+    "basicc addition2"_test = [] {
+        expect(add(3,3) == 5_i);
     };
 }
